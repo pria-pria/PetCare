4170eceb7025b30815f8c42ecc80ffaeb9e16653
Priya <pdugg21@gmail.com>
2026-04-24 15:01:16 +0530

    updated

--- app.py
+++ app.py
@@ -151,8 +151,7 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
-
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run()
